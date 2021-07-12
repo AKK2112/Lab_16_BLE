63f8f5dba09f30b9fbffca69715bdbc18036e5af
AKK2112 <aleckk18@gmail.com>
2021-07-11 21:23:20 -0500

    comments

--- src/Lab_16_BLE.ino
+++ src/Lab_16_BLE.ino
@@ -9,9 +9,9 @@ const BleUuid serviceUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid rxUuid("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
-BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
+BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL); //bluetooth setup stuff
 
-void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context)
+void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context)//defines the onDataRecieved function
 {
   for (size_t i = 0; i < len; i++)
   {
@@ -21,15 +21,17 @@ void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, 
       display.setTextSize(1);
       display.setTextColor(WHITE);
       display.setCursor(0, 0);
-      display.println("Never gonna give you up, Never gonna let you down...");
+      display.println("Never gonna give you up, Never gonna let you down..."); //When a 1 is recieved, it Rick-Rolls the teacher :)
       display.display();
     }
     if (data[0] == '0')
     {
-      RGB.control(true);
+      RGB.control(true); //When a 0 is recieved, it changes the light to yellow and disconnects
       RGB.color(255, 255, 0);
       RGB.brightness(255);
+      delay(100);
       BLE.disconnect();
+      delay(50);
     }
     Serial.write(data[i]);
     
@@ -45,7 +47,7 @@ void setup()
   BLE.addCharacteristic(txCharacteristic);
   BLE.addCharacteristic(rxCharacteristic);
 
-  BleAdvertisingData data;
+  BleAdvertisingData data;//advertising the bluetooth data
   data.appendServiceUUID(serviceUuid);
   BLE.advertise(&data);
 }
@@ -56,16 +58,16 @@ void loop()
   if (BLE.connected())
   {
     uint8_t txBuf[UART_TX_BUF_SIZE];
-    String message = "Hello!\n";
+    String message = "Hello!\n"; //sends hello to the UART panel on my phone.  
     message.toCharArray((char *)txBuf, message.length() + 1);
     txCharacteristic.setValue(txBuf, message.length() + 1);
-    RGB.control(true);
+    RGB.control(true);//after it connects, it changes the RGB to blue
     RGB.color(0, 0, 255);
     RGB.brightness(255);
   }
   else
   {
-    RGB.control(true);
+    RGB.control(true);//if the board isnt connected, it is a yellow light
     RGB.color(255, 255, 0);
     RGB.brightness(255);
   }
